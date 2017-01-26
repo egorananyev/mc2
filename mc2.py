a45864ef316bf4cef5f6cb79d8ac4322b789d0ab
egorananyev <egor.ananyev@gmail.com>
2017-01-26 21:11:54 +0800

    collected and analyzed peri data

--- mc2.py
+++ mc2.py
@@ -27,7 +27,7 @@ _thisDir = os.path.dirname(os.path.abspath(__file__))
 # ====================================================================================
 ## Initial variables.
 et = 0
-expName = 'mc2_tgT-mcBv_test' # can be overwritten through GUI
+expName = 'mc2_tgT-mcBv_cent' # can be overwritten through GUI
 # Window circles (specified in degrees of visual angles [dva]):
 #winSz = 7.2 # 5.03; calculated as 5/x=sqrt(2)/2 => x=10/sqrt(2)
 winOffX = 4.25 # 6 # 5.62
